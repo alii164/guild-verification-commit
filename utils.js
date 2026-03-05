4db6289b2a2f8325c1185bdc17d06f0fb64ea5f4
ali <130998806+alii164@users.noreply.github.com>
2026-03-05 21:00:03 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:59:49.989Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T18:00:02.421Z
+Commit: Patch security vulnerability in authentication
