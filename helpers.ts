8d708257bf2b4f1c6438d1ed8ed1b6110e8c41c3
ali <130998806+alii164@users.noreply.github.com>
2026-03-05 21:13:39 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:13:33.117Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T18:13:39.117Z
+Commit: Introduce real-time notifications
