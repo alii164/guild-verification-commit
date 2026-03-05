9177f4642542f5580d5110dc5491b8cfa80554e6
ali <130998806+alii164@users.noreply.github.com>
2026-03-05 20:59:08 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:59:06.894Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T17:59:08.150Z
+Commit: Resolve null pointer exception in user service
