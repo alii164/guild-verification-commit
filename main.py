e7cd91570a4ed9ad873da3ac17896d06af84d512
ali <130998806+alii164@users.noreply.github.com>
2026-03-05 21:12:55 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:59:57.934Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T18:12:54.680Z
+Commit: Fix memory leak in event handlers
