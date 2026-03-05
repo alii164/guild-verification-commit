b0c3f51e4e93d380936b08df7a801612b0a1bec7
ali <130998806+alii164@users.noreply.github.com>
2026-03-05 20:59:49 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:59:23.166Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-05T17:59:48.679Z
+Commit: Resolve null pointer exception in user service
