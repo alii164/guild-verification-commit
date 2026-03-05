ca6eefd15e81d251bcc3a684b9f38bbe80eae7f6
ali <130998806+alii164@users.noreply.github.com>
2026-03-05 20:54:40 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:53:06.990Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T17:54:39.318Z
+Commit: Patch security vulnerability in authentication
