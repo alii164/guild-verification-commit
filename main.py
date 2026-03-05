22ac4534d994aaab5bc1f27fe5e35c95939274db
ali <130998806+alii164@users.noreply.github.com>
2026-03-05 20:59:58 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:59:19.255Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T17:59:57.934Z
+Commit: Patch API endpoint error handling
