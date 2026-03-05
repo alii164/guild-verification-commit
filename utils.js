50a26c12f191fbc6346fd65923a276751628bdd4
ali <130998806+alii164@users.noreply.github.com>
2026-03-05 21:01:33 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:00:02.421Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T18:01:33.262Z
+Commit: Add multi-language support
